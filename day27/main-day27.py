9b38813c608f88fd98628167271169ec030c1277
Xinxin <zhangxx19@mails.tsinghua.edu.cn>
2023-05-12 15:38:45 +0800

    day27 code halfed

--- day27/main-day27.py
+++ day27/main-day27.py
@@ -1,18 +1,32 @@
 import tkinter
+from tkinter import *
 
-window = tkinter.Tk() # 创建一个窗口
+window = tkinter.Tk()  # 创建一个窗口
 window.title("My First GUI Program")
-window.minsize(width=500, height=300) #设置窗口大小
+window.minsize(width=500, height=300)  # 设置窗口大小
 
-#Label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold")) #创建一个label
-my_label.pack(side='left', expand=True) #把创建的label放进window里, pack是一个排版的工具
-#pack里可以调的参数写在了**kw里面，有无限的参数可以写？
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))  # 创建一个label
+my_label.pack()  # 把创建的label放进window里, pack是一个排版的工具
+# pack里可以调的参数写在了**kw里面，有无限的参数可以写？
 
-import turtle
+# 以下两种方式和上面直接定义的时候设置text都可以改变text的值，都可以用
+my_label["text"] = 'New Text'
+my_label.config(text="New Text")
 
-tim = turtle.Turtle()
-tim.write() #write的参数里写了所有可以调的东西
+
+def button_clicked():
+    my_label["text"] = input.get()
+    print("I got clicked")
 
 
-window.mainloop() #让创建的窗口一直显示
+# Button
+button = Button(text='Click me', command=button_clicked)  # command写点了这个按钮之后要干嘛
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
+
+window.mainloop()  # 让创建的窗口一直显示
